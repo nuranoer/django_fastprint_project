d7cd05b77c775921b531e3bf1604ad4d333111cc
Fitri Zuyina Nur Azizah <nuranoer7@gmail.com>
2023-09-26 17:03:13 +0700

    create model

--- produk/models.py
+++ produk/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Kategori(models.Model):
+    nama_kategori = models.CharField(max_length=100)
+
+class Status(models.Model):
+    nama_status = models.CharField(max_length=100)
+
+class Produk(models.Model):
+    nama_produk = models.CharField(max_length=255)
+    harga = models.DecimalField(max_digits=10, decimal_places=2)
+    kategori = models.ForeignKey(Kategori, on_delete=models.CASCADE)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
